f7b25427a247c035decd086ebc61bd56e847d508
Nikolas de Queiroz <nikolasdequeiroz@gmail.com>
2020-10-05 20:54:36 -0300

    Start of script. Correctly reading the files in the directory

--- process_textfiles/text_processing.py
+++ process_textfiles/text_processing.py
@@ -1,3 +1,23 @@
 import os
 import requests
 
+def process_text(base_dir):
+    feedback_dict = {}
+    keys = ['title', 'name', 'date', 'feedback']
+    for filename in os.listdir(base_dir):
+        with open(os.path.join(base_dir, filename), 'r') as file:
+            for line in file.readlines():
+                print(line.strip())
+
+
+
+
+        file.close()
+
+
+
+
+    return None
+
+
+process_text(r"data/feedback")
