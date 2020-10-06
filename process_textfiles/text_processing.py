9ea527389f4857df748b4328cd04c87812400211
Nikolas de Queiroz <nikolasdequeiroz@gmail.com>
2020-10-05 21:33:36 -0300

    Script correctly creates the Python Dictionary

--- process_textfiles/text_processing.py
+++ process_textfiles/text_processing.py
@@ -1,21 +1,23 @@
 import os
 import requests
 
+
 def process_text(base_dir):
     feedback_dict = {}
+    n = 0
     keys = ['title', 'name', 'date', 'feedback']
     for filename in os.listdir(base_dir):
+        n += 1
+        feedback_dict['id' + str(n)] = {}
         with open(os.path.join(base_dir, filename), 'r') as file:
+            i = 0
             for line in file.readlines():
-                print(line.strip())
-
-
-
-
+                feedback_dict['id' + str(n)][keys[i]] = line.strip()
+                i += 1
         file.close()
 
 
-
+    # print(feedback_dict)
 
     return None
 
