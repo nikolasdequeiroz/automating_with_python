7b10dfab2df76ae6ec49e79d2946c016cb5fc786
Nikolas de Queiroz <nikolasdequeiroz@gmail.com>
2020-10-05 22:20:57 -0300

    Script correctly sends the POST requests to the webserver, in JSON format

--- process_textfiles/text_processing.py
+++ process_textfiles/text_processing.py
@@ -2,24 +2,29 @@ import os
 import requests
 
 
-def process_text(base_dir):
+def process_text(base_dir, webpage_url):
     feedback_dict = {}
-    n = 0
+    n = 0  # Initializing the variable to iterate over the IDs
     keys = ['title', 'name', 'date', 'feedback']
     for filename in os.listdir(base_dir):
         n += 1
-        feedback_dict['id' + str(n)] = {}
+        feedback_dict = {}
         with open(os.path.join(base_dir, filename), 'r') as file:
-            i = 0
+            i = 0  # Initializing the variable to iterate over the keys
             for line in file.readlines():
-                feedback_dict['id' + str(n)][keys[i]] = line.strip()
+                feedback_dict[keys[i]] = line.strip()
                 i += 1
         file.close()
+        response = requests.post(webpage_url, json=feedback_dict)
+
+        if response.status_code == 201:
+            print("Information correctly sent. Status code {}".format(response.status_code))
+        else:
+            print("Error {}. Information not sent".format(response.status_code))
 
 
-    # print(feedback_dict)
-
-    return None
+process_text("/data/feedback/", 'http://serve_ip/feedback/')
 
 
-process_text(r"data/feedback")
+# Example of a function call
+process_text(r"data/feedback", webpage_url=r'http://104.154.70.226/feedback')
