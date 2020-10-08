100a664e67334fce30f532dc4027992516f638f9
Nikolas de Queiroz <nikolasdequeiroz@gmail.com>
2020-10-08 20:40:50 -0300

    First version of script. Not tested yet. This will be tested in the correct environment later

--- update_catalog/supplier_image_upload.py
+++ update_catalog/supplier_image_upload.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 
+import os
+import requests
+
+path = "supplier-data/images/"
+webpage_url = "linux-instance-IP-Address/media/images/"
+
+
+for filename in os.listdir(path):
+    if filename.endswith('.jpeg'):
+        response = requests.post(webpage_url, file={'file': filename})
