385456b92e6fe47936904defb3ec2482990521d7
Nikolas de Queiroz <nikolasdequeiroz@gmail.com>
2020-10-08 20:39:09 -0300

    Script converts and resizes images correctly

--- update_catalog/changeImage.py
+++ update_catalog/changeImage.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 
+
+import os
+from PIL import Image
+
+path = "supplier-data/images/"
+size = (600, 400)
+
+for filename in os.listdir(path):
+    if filename.endswith('.tiff'):
+        raw_filename, ext = os.path.splitext(filename)  # Getting the filename without the extension
+        Image.open(path + filename).convert('RGB').resize(size).save(path + raw_filename + '.jpeg', 'JPEG')
+
