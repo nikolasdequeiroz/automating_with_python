7fc045ec48f40b9a8a1e6ae44ef9060ca3480fc3
Nikolas de Queiroz <nikolasdequeiroz@gmail.com>
2020-10-08 23:50:43 -0300

    First commit of report_email script. The code is under development

--- update_catalog/report_email.py
+++ update_catalog/report_email.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+
+"""
+Once you define the generate_email and send_email methods
+ call the methods under the main method after creating the PDF report:
+ """
+
+
+def main():
+    return
+
+
+"""
+if __name__ == "__main__":
+    
+    
+"""
