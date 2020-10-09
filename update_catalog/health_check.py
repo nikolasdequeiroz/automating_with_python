34097d0e12a4b43a136dd65833f1bbc838df3d6a
Nikolas de Queiroz <nikolasdequeiroz@gmail.com>
2020-10-08 23:51:33 -0300

    First commit of health_check script. The code is under development

--- update_catalog/health_check.py
+++ update_catalog/health_check.py
@@ -3,3 +3,14 @@
 import shutil
 import psutil
 
+
+# Report an error if CPU usage is over 80%
+
+
+# Report an error if available disk space is lower than 20%
+
+
+# Report an error if available memory is less than 500MB
+
+
+# Report an error if the hostname cannot be resolved to 127.0.0.1
