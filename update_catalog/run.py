a4722dc906499ee84255517467f8f6ec8c025836
Nikolas de Queiroz <nikolasdequeiroz@gmail.com>
2020-10-10 11:19:16 -0300

    Correction of some bugs. Images correct related with description

--- update_catalog/run.py
+++ update_catalog/run.py
@@ -5,36 +5,23 @@ import requests
 
 description_path = "supplier-data/descriptions/"
 image_path = "supplier-data/images/"
-url = "http://localhost/fruits/"
+webpage_url = "http://localhost/fruits/"
 dict_keys = ['name', 'weight', 'description', 'image_name']
-images_list = []
 
-
-# Creating a list to store the images names
-images_list = [image_name for image_name in os.listdir(image_path) if image_name.endswith('jpeg')]
-img_itr = 0
 
 # Iterating over all files in the descriptions directory
 for filename in os.listdir(description_path):
     with open(description_path + filename, 'r') as file:
-        fruits_dict = {}
-        key_itr = 0
-        for line in file.readlines():
-            if 'lbs' in line:
-                fruits_dict[dict_keys[key_itr]] = int(line.strip().split()[0])
-                key_itr += 1
-            else:
-                fruits_dict[dict_keys[key_itr]] = line.strip()
-                key_itr += 1
+        with open(description_path + filename, 'r') as f:
+            fruit_name = os.path.splitext(filename)[0]
+            data = f.read()
+            data = data.split('\n')
+            fruits_dict = {"name": data[0], "weight": int(data[1].split()[0]), "description": data[2],
+                           "image_name": fruit_name + ".jpeg"}
 
-    # Attaching the corresponding image name iterating through image_names list
-    fruits_dict['image_name'] = images_list[img_itr]
-    img_itr += 1
+            response = requests.post(webpage_url, json=fruits_dict)
 
     print(fruits_dict)
     file.close()
     # Posting the dictionary in the webpage using json format
     # response = requests.post(webpage_url, json=fruits_dict)
-
-
-
